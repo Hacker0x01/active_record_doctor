428db1c86e774c85cb3a4f6ec6a447b2d620bb87
Alexey Zolotov <azolotov@hackerone.com>
2020-07-09 16:26:48 +0200

    Ignore ar_internal_darta

--- lib/active_record_doctor/tasks/missing_string_length_validation.rb
+++ lib/active_record_doctor/tasks/missing_string_length_validation.rb
@@ -9,7 +9,7 @@ module ActiveRecordDoctor
         eager_load!
 
         success(hash_from_pairs(models.reject do |model|
-          model.table_name.nil? || model.table_name == 'schema_migrations' || !model.table_exists?
+          model.table_name.nil? || model.table_name == 'schema_migrations' || model.table_name == 'ar_internal_metadata' || !model.table_exists?
         end.map do |model|
           [
             model.name,
